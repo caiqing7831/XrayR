bdad4436da70097d514db7c3614d6a7a6c2adb48
suxiyun <95904036+suxiyun@users.noreply.github.com>
2022-04-28 10:33:01 +0800

    Update config.go

--- panel/config.go
+++ panel/config.go
@@ -1,8 +1,8 @@
 package panel
 
 import (
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/service/controller"
+	"github.com/suxiyun/XrayR/api"
+	"github.com/suxiyun/XrayR/service/controller"
 )
 
 type Config struct {
